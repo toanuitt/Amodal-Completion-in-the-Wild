cc1a4f164c8e486513f086b25510e8266c36e5a9
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-15 22:02:42 +0700

    make input_path and output_path as CLI arguments

--- dift/extract_dift_amodal.py
+++ dift/extract_dift_amodal.py
@@ -1,73 +1,112 @@
 import argparse
+import os
 import torch
 from PIL import Image
 from torchvision.transforms import PILToTensor
 from src.models.dift_sd import SDFeaturizer
-import os
-import json
-import ipdb
+
+
+def is_image(file_name: str) -> bool:
+    file_name = file_name.lower()
+    return file_name.endswith(".jpg") or file_name.endswith(".png")
+
+
+def get_image_tensor(image_path: str, image_size: list):
+    img = Image.open(image_path).convert("RGB")
+    if image_size[0] != -1:
+        img = img.resize(image_size)
+    img_tensor = (PILToTensor()(img) / 255.0 - 0.5) * 2
+
+    return img_tensor
+
 
 def main(args):
     dift = SDFeaturizer(args.model_id)
-    img = Image.open(args.input_path).convert('RGB')
-    if args.img_size[0] > 0:
-        img = img.resize(args.img_size)
-    img_tensor = (PILToTensor()(img) / 255.0 - 0.5) * 2
-    ft = dift.forward(img_tensor,
-                      prompt=args.prompt,
-                      t=args.t,
-                      up_ft_index=args.up_ft_index,
-                      ensemble_size=args.ensemble_size)
-    return ft
+    img_path = args.input_path
+    save_path = args.output_path
 
+    for img_name in os.listdir(img_path):
+        if not is_image(img_name):
+            continue
 
-if __name__ == '__main__':
+        img_path = os.path.join(img_path, img_name)
+        img_tensor = get_image_tensor(img_path, args.img_size)
+        ft = dift.forward(
+            img_tensor,
+            prompt=args.prompt,
+            t=args.t,
+            up_ft_index=args.up_ft_index,
+            ensemble_size=args.ensemble_size,
+        )
 
-    parser = argparse.ArgumentParser(
-        description='''extract dift from input image, and save it as torch tenosr,
-                    in the shape of [c, h, w].''')
-    
-    parser.add_argument('--img_size', nargs='+', type=int, default=[768, 768],
-                        help='''in the order of [width, height], resize input image
-                            to [w, h] before fed into diffusion model, if set to 0, will
-                            stick to the original input size. by default is 768x768.''')
-    parser.add_argument('--model_id', default='stabilityai/stable-diffusion-2-1', type=str, 
-                        help='model_id of the diffusion model in huggingface')
-    parser.add_argument('--t', default=261, type=int, 
-                        help='time step for diffusion, choose from range [0, 1000]')
-    parser.add_argument('--up_ft_index', default=1, type=int, choices=[0, 1, 2 ,3],
-                        help='which upsampling block of U-Net to extract the feature map')
-    parser.add_argument('--prompt', default='', type=str,
-                        help='prompt used in the stable diffusion')
-    parser.add_argument('--ensemble_size', default=8, type=int, 
-                        help='number of repeated images in each batch used to get features')
-    parser.add_argument('--input_path', type=str,
-                        help='path to the input image file')
-    parser.add_argument('--output_path', type=str, default='dift.pt',
-                        help='path to save the output features as torch tensor')
-    args = parser.parse_args()
-
-    t = 181
-    print(t)
-
-
-    img_dir = '' # fill in the path to the image directory
-    save_dir = '' # fill in the path to the directory of saving the extracted features
-    img_name_list = json.load(open('')) # fill in the path to the image name list
-
-    for img_name in img_name_list:
-        print(img_name)
-        args.t = t
-        args.up_ft_index = 1
-        args.input_path = os.path.join(img_dir, img_name)
-        args.prompt = ''
-        ft = main(args)
         for key_i in ft.keys():
-            cur_folder = os.path.join(save_dir, 't_' + str(t) + '_up-ft-index_' + str(key_i))
+            cur_folder = os.path.join(
+                save_path, "t_" + str(args.t) + "_index_" + str(key_i)
+            )
             if not os.path.exists(cur_folder):
-                os.mkdir(cur_folder)
-            args.output_path = os.path.join(cur_folder, img_name[:-4] + '.pt')
-            cur_ft = torch.save(ft[key_i].squeeze(0).cpu(), args.output_path)
+                os.makedirs(cur_folder)
+            tensor_file_name = os.path.join(cur_folder, img_name[:-4] + ".pt")
+            torch.save(ft[key_i].squeeze(0).cpu(), tensor_file_name)
 
 
-    
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="""extract dift from input image, and save it as torch tensor,
+                    in the shape of [c, h, w]."""
+    )
+    parser.add_argument(
+        "--img_size",
+        nargs="+",
+        type=int,
+        default=[768, 768],
+        help="""in the order of [width, height], resize input image
+                            to [w, h] before fed into diffusion model, if set to 0, will
+                            stick to the original input size. by default is 768x768.""",
+    )
+    parser.add_argument(
+        "--model_id",
+        default="stabilityai/stable-diffusion-2-1",
+        type=str,
+        help="model_id of the diffusion model in huggingface",
+    )
+    parser.add_argument(
+        "--t",
+        default=181,
+        type=int,
+        help="time step for diffusion, choose from range [0, 1000]",
+    )
+    parser.add_argument(
+        "--up_ft_index",
+        default=1,
+        type=int,
+        choices=[0, 1, 2, 3],
+        help="which upsampling block of U-Net to extract the feature map",
+    )
+    parser.add_argument(
+        "--prompt",
+        default="",
+        type=str,
+        help="prompt used in the stable diffusion",
+    )
+    parser.add_argument(
+        "--ensemble_size",
+        default=8,
+        type=int,
+        help="number of repeated images in each batch used to get features",
+    )
+    parser.add_argument(
+        "--input_path", type=str, help="path to the input image file"
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default="dift.pt",
+        help="path to save the output features as torch tensor",
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
