104563332828bc6caaed3b94f6d0dea7d012fa7a
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-17 21:11:39 +0700

    debug

--- dift/extract_dift_amodal.py
+++ dift/extract_dift_amodal.py
@@ -7,54 +7,6 @@ from torchvision.transforms import PILToTensor
 from tqdm import tqdm
 
 
-def is_image(file_name: str) -> bool:
-    file_name = file_name.lower()
-    return file_name.endswith(".jpg") or file_name.endswith(".png")
-
-
-def get_image_tensor(image_path: str, image_size: list):
-    img = Image.open(image_path).convert("RGB")
-    if image_size[0] != -1:
-        img = img.resize(image_size)
-
-    img_tensor = (PILToTensor()(img) / 255.0 - 0.5) * 2
-
-    return img_tensor
-
-
-def main(args):
-    dift = SDFeaturizer(sd_id=args.model_id)
-    imgs_path = args.input_path
-    save_path = args.output_path
-
-    # for img_name in tqdm(os.listdir(imgs_path)):
-    for img_name in os.listdir(imgs_path):
-        if not is_image(img_name):
-            continue
-
-        img_path = os.path.join(imgs_path, img_name)
-        img_tensor = get_image_tensor(img_path, args.img_size)
-        ft = dift.forward(
-            img_tensor,
-            prompt=args.prompt,
-            t=args.t,
-            up_ft_index=args.up_ft_index,
-            ensemble_size=args.ensemble_size,
-        )
-
-        print(f"{img_name=}")
-        # print(f"{ft}")
-
-        for key_i in ft.keys():
-            cur_folder = os.path.join(
-                save_path, "t_" + str(args.t) + "_index_" + str(key_i)
-            )
-            if not os.path.exists(cur_folder):
-                os.makedirs(cur_folder)
-            tensor_file_name = os.path.join(cur_folder, img_name[:-4] + ".pt")
-            torch.save(ft[key_i].squeeze(0).cpu(), tensor_file_name)
-
-
 def parse_args():
     parser = argparse.ArgumentParser(
         description="""extract dift from input image, and save it as torch tensor,
@@ -111,6 +63,53 @@ def parse_args():
     )
     args = parser.parse_args()
     return args
+
+
+def is_image(file_name: str) -> bool:
+    file_name = file_name.lower()
+    return file_name.endswith(".jpg") or file_name.endswith(".png")
+
+
+def get_image_tensor(image_path: str, image_size: list):
+    img = Image.open(image_path).convert("RGB")
+    if image_size[0] != -1:
+        img = img.resize(image_size)
+
+    img_tensor = (PILToTensor()(img) / 255.0 - 0.5) * 2
+
+    return img_tensor
+
+
+def main(args):
+    dift = SDFeaturizer(sd_id=args.model_id)
+    imgs_path = args.input_path
+    save_path = args.output_path
+
+    # for img_name in tqdm(os.listdir(imgs_path)):
+    for img_name in os.listdir(imgs_path):
+        if not is_image(img_name):
+            continue
+
+        img_path = os.path.join(imgs_path, img_name)
+        img_tensor = get_image_tensor(img_path, args.img_size)
+        ft = dift.forward(
+            img_tensor,
+            prompt=args.prompt,
+            t=args.t,
+            up_ft_index=args.up_ft_index,
+            ensemble_size=args.ensemble_size,
+        )
+
+        print(f"{img_name=}")
+        # print(f"{ft}")
+
+        cur_folder = os.path.join(
+            save_path, "t_" + str(args.t) + "_index_" + str(args.up_ft_index)
+        )
+        if not os.path.exists(cur_folder):
+            os.makedirs(cur_folder)
+        tensor_file_name = os.path.join(cur_folder, img_name[:-4] + ".pt")
+        torch.save(ft.squeeze(0).cpu(), tensor_file_name)
 
 
 if __name__ == "__main__":
