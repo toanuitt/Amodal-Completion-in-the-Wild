c61cb2d65a68eea5b1ba792e68083fbdb097f681
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-20 20:21:32 +0700

    add extract_dift with annotations

--- tools/create_dataset.py
+++ tools/create_dataset.py
@@ -1,63 +1,134 @@
 import os
 import json
-from PIL import Image
-from shapely import Polygon
 import numpy as np
-import pycocotools.mask as maskUtils
 import cv2
+import copy
+from tqdm import tqdm
 
 
-def binary_mask_to_rle(binary_mask):
-    rle = {"counts": [], "size": list(binary_mask.shape)}
-    counts = rle.get("counts")
-
-    last_elem = 0
-    running_length = 0
-
-    for _, elem in enumerate(binary_mask.ravel(order="F")):
-        if elem == last_elem:
-            pass
-        else:
-            counts.append(running_length)
-            running_length = 0
-            last_elem = elem
-        running_length += 1
-
-    counts.append(running_length)
-
-    return rle
+def get_mask_area(mask, threshold):
+    return np.sum(mask > threshold)
 
 
-def binary_mask_to_rle2(binary_mask):
-    rle = {"counts": [], "size": list(binary_mask.shape)}
-    counts = rle.get("counts")
-    for i, (value, elements) in enumerate(
-        groupby(binary_mask.ravel(order="F"))
-    ):
-        if i == 0 and value == 1:
-            counts.append(0)
-        counts.append(len(list(elements)))
-    return rle
+def split_mask(mask, split_percents, error_percent=0.05):
+    h, w = mask.shape[:2]
+    left_mask = np.zeros([h, w])
+    right_mask = mask.copy()
+    mask_area = get_mask_area(mask, 128)
+
+    split_result = {}
+    curr_masks = {
+        "left_mask": [],
+        "right_mask": [],
+        "percent": 0,
+        "last_col": 0,
+    }
+
+    curr_pos = 0
+    curr_percent = split_percents[curr_pos]
+    last_left_percent = 0
+    for col in range(w):
+        left_mask[0:h, col][right_mask[0:h, col] > 128] = 255
+        right_mask[0:h, col] = 0
+
+        left_mask_area = get_mask_area(left_mask, 128)
+        left_mask_area_percent = left_mask_area / mask_area
+
+        low_threshold = curr_percent - error_percent
+        high_threshold = curr_percent + error_percent
+        if left_mask_area_percent < low_threshold:
+            continue
+
+        if left_mask_area_percent > high_threshold:
+            split_result[curr_percent] = copy.deepcopy(curr_masks)
+            curr_pos += 1
+            if curr_pos == len(split_percents):
+                break
+            curr_percent = split_percents[curr_pos]
+            continue
+
+        diff_last = abs(curr_percent - last_left_percent)
+        diff_curr = abs(curr_percent - left_mask_area_percent)
+
+        if diff_last > diff_curr:
+            last_left_percent = left_mask_area_percent
+            curr_masks["left_mask"] = left_mask.copy()
+            curr_masks["right_mask"] = right_mask.copy()
+            curr_masks["percent"] = left_mask_area_percent
+            curr_masks["last_col"] = col
+
+    return split_result
+
+
+def convert_mask_to_polygon(mask):
+    mask = mask.astype(np.uint8)
+    contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    polygons = [contour.squeeze().tolist() for contour in contours]
+    return polygons
 
 
 if __name__ == "__main__":
     image_root = "D:/HocTap/KLTN/dataset/processed/mask_create/300"
+    anno_save_path = "test.json"
     split_percents = [0.3, 0.5, 0.7]
 
-    total = 0
-    for image_name in os.listdir(image_root):
+    content = {"images": [], "annotations": []}
+    image_idx = 1
+    anno_idx = 1
+    for image_name in tqdm(os.listdir(image_root)):
         if "mask" in image_name:
             continue
 
         image_path = f"{image_root}/{image_name}"
-        mask_name = f"{image_name.split('.')[0]}_mask.jpg"
+        image_name_wo_ext = image_name.split(".")[0]
+        mask_name = f"{image_name_wo_ext}_mask.jpg"
         mask_path = f"{image_root}/{mask_name}"
 
         assert os.path.exists(mask_path), f"Cannot find {mask_path=}"
 
-        mask = np.array(Image.open(mask_path))
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        _, mask = cv2.threshold(mask, 127, 255, 0)
 
-        # rle = maskUtils.encode(np.asfortranarray(mask))
-        # rle_mask = maskUtils.decode(rle).squeeze()
+        image_info = {
+            "id": image_idx,
+            "file_name": image_name,
+            "width": mask.shape[1],
+            "height": mask.shape[0],
+        }
 
-    print(total / len(os.listdir(image_root)))
+        content["images"].append(image_info)
+
+        results = split_mask(mask, split_percents)
+
+        all_polygons = convert_mask_to_polygon(mask)
+
+        for value in results.values():
+            left_polygons = convert_mask_to_polygon(value["left_mask"])
+            right_polygons = convert_mask_to_polygon(value["right_mask"])
+
+            anno = {
+                "id": anno_idx,
+                "image_id": image_idx,
+                "last_col": value["last_col"],
+                "segmentations": all_polygons,
+                "visible_segmentations": left_polygons,
+                "invisible_segmentations": right_polygons,
+            }
+            content["annotations"].append(anno)
+            anno_idx += 1
+
+            anno = {
+                "id": anno_idx,
+                "image_id": image_idx,
+                "last_col": -value["last_col"],
+                "segmentations": all_polygons,
+                "visible_segmentations": right_polygons,
+                "invisible_segmentations": left_polygons,
+            }
+            content["annotations"].append(anno)
+            anno_idx += 1
+
+        image_idx += 1
+
+    with open(anno_save_path, "w+") as anno_file:
+        json.dump(content, anno_file)
---
+++ tools/extract_dift_amodal_anno.py
@@ -0,0 +1,171 @@
+import argparse
+import os
+import sys
+import json
+
+import torch
+from PIL import Image
+from torchvision.transforms import PILToTensor
+from tqdm import tqdm
+import numpy as np
+
+sys.path.append(".")
+from libs.dift.dift_sd import SDFeaturizer
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="""extract dift from input image, and save it as torch tensor,
+                    in the shape of [c, h, w]."""
+    )
+    parser.add_argument(
+        "--img_size",
+        nargs="+",
+        type=int,
+        default=[768, 768],
+        help="""in the order of [width, height], resize input image
+                            to [w, h] before fed into diffusion model, if set to 0, will
+                            stick to the original input size. by default is 768x768.""",
+    )
+    parser.add_argument(
+        "--model-id",
+        default="stabilityai/stable-diffusion-2-1",
+        type=str,
+        help="model_id of the diffusion model in huggingface",
+    )
+    parser.add_argument(
+        "--t",
+        default=181,
+        type=int,
+        help="time step for diffusion, choose from range [0, 1000]",
+    )
+    parser.add_argument(
+        "--prompt",
+        default="",
+        type=str,
+        help="prompt used in the stable diffusion",
+    )
+    parser.add_argument(
+        "--ensemble-size",
+        default=8,
+        type=int,
+        help="number of repeated images in each batch used to get features",
+    )
+    parser.add_argument(
+        "--input-path", type=str, help="path to the input image file"
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        default="dift.pt",
+        help="path to save the output features as torch tensor",
+    )
+    parser.add_argument(
+        "--start-index",
+        type=int,
+        default=0,
+        help="the first index of first image to take feature",
+    )
+    parser.add_argument(
+        "--no-image",
+        type=int,
+        default=1,
+        help="the number of images to take feature",
+    )
+    parser.add_argument(
+        "--anno-path",
+        type=str,
+        default=1,
+        help="the path to json annotation file",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def is_image(file_name: str) -> bool:
+    file_name = file_name.lower()
+    return file_name.endswith(".jpg") or file_name.endswith(".png")
+
+
+def get_image_tensor(image_path: str, image_size: list):
+    img = Image.open(image_path).convert("RGB")
+    if image_size[0] != -1:
+        img = img.resize(image_size)
+
+    img_tensor = (PILToTensor()(img) / 255.0 - 0.5) * 2
+
+    return img_tensor
+
+
+def get_annos(anno_path):
+    with open(anno_path, "r") as anno_file:
+        content = json.load(anno_file)
+
+    image_dict = dict(
+        [[img_info["id"], img_info] for img_info in content["images"]]
+    )
+
+    annos = []
+    for anno in content["annotations"]:
+        img_info = image_dict[anno["image_id"]]
+        black_start = 0
+        black_end = 0
+        if anno["last_col"] > 0:
+            black_start = anno["last_col"]
+            black_end = img_info["width"]
+        else:
+            black_end = anno["last_col"] + 1
+
+        annos.append(
+            {
+                "id": anno["id"],
+                "file_name": img_info,
+                "image_height": img_info["height"],
+                "black_start": black_start,
+                "black_end": black_end,
+            }
+        )
+
+    return annos
+
+
+def main(args):
+    dift = SDFeaturizer(sd_id=args.model_id)
+    up_ft_indices = [0, 1, 2, 3]
+    imgs_path = args.input_path
+    save_path = args.output_path
+
+    annos = get_annos(args.anno_path)
+
+    for anno in tqdm(annos):
+        img_path = os.path.join(imgs_path, anno["file_name"])
+        img = np.array(Image.open(img_path))
+        img[
+            0 : anno["image_height"], anno["black_start"] : anno["black_end"]
+        ] = [0, 0, 0]
+        img_tensor = (torch.Tensor([img]) / 255.0 - 0.5) * 2
+
+        fts = dift.forward(
+            img_tensor=img_tensor,
+            prompt=args.prompt,
+            t=args.t,
+            up_ft_indices=up_ft_indices,
+            ensemble_size=args.ensemble_size,
+        )
+
+        for key, value in fts.items():
+            cur_folder = os.path.join(
+                save_path, "t_" + str(args.t) + "_index_" + str(key)
+            )
+            if not os.path.exists(cur_folder):
+                os.makedirs(cur_folder)
+
+            tensor_file_name = os.path.join(
+                cur_folder, f"{anno['file_name'][:-4]}_{anno['id']}_.pt"
+            )
+            torch.save(value.squeeze(0).cpu(), tensor_file_name)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
